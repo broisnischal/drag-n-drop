57aafc40c8ac82806844a4a37409a52a850c5257
broisnischal <neeswebservice@gmail.com>
2024-03-23 19:34:18 +0545

    feat:configuring-package

--- packages/drag-n-drop/src/drag.tsx
+++ packages/drag-n-drop/src/drag.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 type ButtonProps = React.HTMLAttributes<ButtonProps> & {
   size?: "small" | "medium" | "large";
   variant?: "primary" | "secondary";
 };
 
-export default function Button(props: ButtonProps) {
+export default function ButtonD(props: ButtonProps) {
   return (
     <button
       className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${props.className}`}
